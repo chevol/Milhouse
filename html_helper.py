9907f5a40b40e0def2db216394fc19f67974e0a4
Chevol Davis <chevol@chevol.com>
2021-02-17 16:20:40 -0500

    Added basic Python logging to Milhouse

--- html_helper.py
+++ html_helper.py
@@ -1,4 +1,7 @@
 import requests
+import logging
+
+logging.basicConfig(filename='milhouse.log',format='%(asctime)s - %(message)s', datefmt='%B %d, %Y %I:%M:%S %p')
 
 class HtmlParser:
     def __init__(self,page_url,headers):
@@ -8,5 +11,7 @@ class HtmlParser:
     def get_page_html(self):
         headers = self.site_headers
         page = requests.get(self.page_url,headers=headers)
-        print("URL Status: " + str(page.status_code))
+        if not str(page.status_code) == '200':
+            logging.error(f'Get Page HTML Error - URL ({self.page_url}) Returned a {str(page.status_code)} Status Code')
+        print('URL Status: ' + str(page.status_code))
         return page.content
--- milhouse.py
+++ milhouse.py
@@ -3,22 +3,26 @@ import walmart
 import target
 import bestbuy
 import amazon
+import logging
+
+logging.basicConfig(filename='milhouse.log', format='%(asctime)s - %(message)s', datefmt='%B %d, %Y %I:%M:%S %p')
 
 def check_inventory():
     walmartchecker = walmart.WalmartChecker()
     targetchecker = target.TargetChecker()
     bestbuychecker = bestbuy.BestBuyChecker()
-    amazonchecker = amazon.AmazonChecker()
+    #amazonchecker = amazon.AmazonChecker()
 
     walmartchecker.check_inventory()
     targetchecker.check_inventory()
     bestbuychecker.check_inventory()
-    amazonchecker.check_inventory()
+    #amazonchecker.check_inventory()
 
 while True:
-    print("----------------------------------------------")
     try:
+        logging.warning('Starting Milhouse Stock Checker')
         check_inventory()
+        logging.warning('Finished Checking Stock')
         time.sleep(165)
-    except:
-        print("An error occured trying to check inventory")
+    except Exception as e:
+        logging.error("An error occured trying to check inventory", exc_info=True)
